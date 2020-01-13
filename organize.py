af98a676817608bbe27488434f48616972e1d012
Andrey Zhidenkov <pensnarik@gmail.com>
2020-01-13 21:35:35 +0100

    Advanced logic for moving file, added year to the file path

--- organize.py
+++ organize.py
@@ -3,14 +3,18 @@
 """
 Scans the source folder for images and moves them into the destination folder
 accorind to their EXIFs and timestamps.
+
+organize.py never overwrites existing files in --dst
 """
 
 import os
 import re
 import sys
 import shutil
+import logging
 import argparse
 import datetime
+from hashlib import md5 as md5_hash
 from datetime import datetime as dt
 from calendar import monthrange
 
@@ -20,7 +24,11 @@ from PIL.JpegImagePlugin import JpegImageFile
 
 EXCLUDE_EXIF = ['MakerNote', 'UserComment']
 MAPPING = {'Xiaomi Redmi Note 7': 'Xiaomi', 'LG Electronics LG-H845': 'LG G5',
-           'Apple iPhone 4S': 'iPhone 4S'}
+           'Apple iPhone 4S': 'iPhone 4S', 'HighScreen Boost IIse': 'HighScreen',
+           'LG Electronics LG-D802': 'LG G2'}
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("organize")
 
 class FileProcessException(Exception):
     pass
@@ -31,10 +39,13 @@ class App():
         parser = argparse.ArgumentParser(description='Image organize tool')
         parser.add_argument('--path', type=str, help='Path', required=True)
         parser.add_argument('--dst', type=str, help='Destination', required=True)
+        parser.add_argument('--test', action='store_true', default=False)
         self.args = parser.parse_args()
 
         if not os.path.exists(self.args.dst) or not os.path.isdir(self.args.dst):
             raise Exception('Destination path does not exist')
+
+        logger.setLevel(logging.ERROR)
 
     def exif2text(self, value):
         """ Helper function """
@@ -50,7 +61,7 @@ class App():
         return result.replace('\u0000', '')
 
     def get_datetime_from_exif(self, exif):
-        if 'DateTimeOriginal' not in exif.keys():
+        if exif is None or 'DateTimeOriginal' not in exif.keys():
             return None
 
         expr_list = {'^(\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2})$': '%Y:%m:%d %H:%M:%S',
@@ -65,22 +76,37 @@ class App():
 
         return d
 
-    def get_device_name(self, fullname):
-        return MAPPING.get(fullname, fullname)
-
-    def get_time_interval(self, filename, exif):
+    def get_datetime_from_filename(self, filename):
         basename = os.path.basename(filename)
         expr_list = {'IMG_(\d{8}_\d{6}).(jpg|jpeg)': '%Y%m%d_%H%M%S',
                      '(\d{8}_\d{6}).(jpg|jpeg)': '%Y%m%d_%H%M%S',
                      '(\d{8}_\d{6})_HDR.(jpg|jpeg)': '%Y%m%d_%H%M%S',
-                     'PANO_(\d{8}_\d{6}).(jpg|jpeg)': '%Y%m%d_%H%M%S'}
+                     'PANO_(\d{8}_\d{6}).(jpg|jpeg)': '%Y%m%d_%H%M%S',
+                     '(VID|video?)_(\d{8}_\d{6}).(mp4)': '%Y%m%d_%H%M%S',
+                     '(\d{8}_\d{6}).(mp4)': '%Y%m%d_%H%M%S',
+                     '(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}).jpg': '%Y-%m-%d_%H-%M-%S'}
         d = None
 
         for expr in expr_list.keys():
             m = re.search(expr, basename)
             if m is not None:
-                d = dt.strptime(m.group(1), expr_list[expr])
+                try:
+                    d = dt.strptime(m.group(1), expr_list[expr])
+                except ValueError:
+                    raise FileProcessException("Invalid value for date and time in filename")
                 break
+
+        return d
+
+    def get_datetime(self, filename, exif):
+        return self.get_datetime_from_exif(exif) or \
+               self.get_datetime_from_filename(filename)
+
+    def get_device_name(self, fullname):
+        return MAPPING.get(fullname, fullname)
+
+    def get_time_interval(self, filename, exif):
+        d = self.get_datetime_from_filename(filename)
 
         if d is None:
             # Try to get date and time from EXIF
@@ -113,29 +139,77 @@ class App():
 
         return exif
 
+    def filter(self, filename):
+        allowed_extensions = ['jpg', 'jpeg', 'mov', 'mp4', 'webm']
+        return any(filename.endswith(i) for i in allowed_extensions)
+
     def get_next_file(self):
         for root, dirs, files in os.walk(self.args.path):
             for file in files:
-                yield os.path.join(root, file)
+                if self.filter(os.path.join(root, file)):
+                    yield os.path.join(root, file)
+
+    def get_make(self, exif):
+        return exif.get('Make') or exif.get('Camera make')
+
+    def get_model(self, exif):
+        return exif.get('Model') or exif.get('Camera model')
+
+    def get_file_hash(self, filename):
+        with open(filename, 'rb') as f:
+            md5 = md5_hash(f.read()).hexdigest()
+        return md5
+
+    def move_file(self, src, dst):
+        def add_index_to_filename(filename, index):
+            return re.sub('(\.)([^\.]+)$', r'__%0.4d.\2' % index, filename)
+
+        if not os.path.exists(dst):
+            # If dst doesn't exist just move src to dst
+            logger.info('MV %s -> %s', src, dst)
+            shutil.move(src, dst)
+        elif self.get_file_hash(src) == self.get_file_hash(dst):
+            # If it exists and it's the same file - remove src
+            logger.info('RM %s' % src)
+            os.remove(src)
+        else:
+            # If both are exist and differ - move with a different name
+            i = 2
+            while os.path.exists(add_index_to_filename(dst, i)):
+                i += 1
+            logger.info('MV %s -> %s', src, add_index_to_filename(dst, i))
+            shutil.move(src, add_index_to_filename(dst, i))
 
     def process_file(self, filename):
         exif = self.get_exif(filename)
+        datetime = self.get_datetime(filename, exif)
+
+        if datetime is None:
+            logger.error("Could not get date and time for file %s", filename)
+            return
+
         basename = os.path.basename(filename)
-        full_device_name = '%s %s' % (exif.get('Make'), exif.get('Model'))
+        full_device_name = '%s %s' % (self.get_make(exif), self.get_model(exif))
         time_interval = self.get_time_interval(filename, exif)
         dst_path = '%s - %s' % (time_interval, self.get_device_name(full_device_name))
-        dst_full_path = os.path.join(self.args.dst, dst_path)
-        if not os.path.exists(dst_full_path):
-            os.mkdir(dst_full_path)
-        print('%s -> %s' % (filename, os.path.join(dst_full_path, basename)))
-        shutil.move(filename, os.path.join(dst_full_path, basename))
+        dst_full_path = os.path.join(self.args.dst, datetime.strftime('%Y'), dst_path)
+
+        if not os.path.exists(dst_full_path) and not self.args.test:
+            os.makedirs(dst_full_path)
+        if not self.args.test:
+            self.move_file(filename, os.path.join(dst_full_path, basename))
+        else:
+            logger.info('%s -> %s' % (filename, os.path.join(dst_full_path, basename)))
 
     def run(self):
         for file in self.get_next_file():
             try:
                 self.process_file(file)
             except FileProcessException:
-                print('Skipping %s' % file)
+                logger.error('Skipping %s' % file)
+            except:
+                logger.error("Unhandled exception while processing file %s" % file)
+                raise
 
 if __name__ == '__main__':
     app = App()
