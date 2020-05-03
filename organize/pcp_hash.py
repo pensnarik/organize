e1f542882fc038b9888c6517b093b50c1683d70c
Andrey Zhidenkov <pensnarik@gmail.com>
2020-05-03 17:42:30 +0200

    Update pcp_hash.py

--- organize/pcp_hash.py
+++ organize/pcp_hash.py
@@ -7,6 +7,15 @@ import subprocess
 
 from PIL import Image
 
+# Size of image perceptive hash, in bits, required to meet 2 conditions:
+# 1) Should be power of 2
+# 2) log(PCP_HASH_SIZE, 2) should be interger
+# Possible values are: 2, 4, 16, 64, 256, 1024, 4096, 16384, ...
+PCP_HASH_SIZE = 256
+
+PCP_THUMB_SIZE = int(math.sqrt(PCP_HASH_SIZE))
+PCP_BITS_PER_ROW = PCP_THUMB_SIZE
+
 def get_file_size(filename):
     statinfo = os.stat(filename)
     return statinfo.st_size
@@ -17,21 +26,30 @@ def get_file_hash(filename):
         md5 = hashlib.md5(f.read()).hexdigest()
     return md5
 
+def get_hash_filename(md5_hash):
+    return os.path.join(get_pcp_hash_dir(), '%s.hash' % md5_hash)
+
 def get_pcp_hash_dir():
     return os.path.expanduser("~/.pcp_hash")
 
 def get_pcp_hash(filename):
+    # TODO
+    # Consider referring to this for more optimal algorithm
+    # http://www.ruanyifeng.com/blog/2011/07/imgHash.txt?spm=a2c65.11461447.0.0.1c8c3588zsrYlA&file=imgHash.txt
     md5_hash = get_file_hash(filename)
 
-    command = 'convert -depth 8 -strip -type Grayscale -geometry 8x8! "%s" "%s/%s.png"' % \
-              (filename, get_pcp_hash_dir(), md5_hash)
+    if os.path.exists(get_hash_filename(md5_hash)):
+        return open(get_hash_filename(md5_hash), 'rt').read()
+
+    command = 'convert -depth 8 -strip -type Grayscale -geometry %sx%s! "%s" "%s/%s.png"' % \
+              (PCP_THUMB_SIZE, PCP_THUMB_SIZE, filename, get_pcp_hash_dir(), md5_hash)
 
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError:
-        return '0000000000000000'
+        return None
 
-    im = Image.open('%s/%s.png' % (get_pcp_hash_dir(), md5_hash,))
+    im = Image.open('%s/%s.png' % (get_pcp_hash_dir(), md5_hash))
     width, height = im.size
 
     if width != height:
@@ -52,11 +70,18 @@ def get_pcp_hash(filename):
         bits = 0
         for x in range(0, height):
             bit = int(im.getpixel((x, y)) > average)
-            bits += bit * math.pow(2, 7 - x)
+            bits += bit * math.pow(2, PCP_BITS_PER_ROW - 1 - x)
+
         hash.append(int(bits))
 
-    hash_as_str = ''.join(['%.2x' % i for i in hash])
+    # We need (PCP_BITS_PER_ROW / 8 * 2) symbols to store a row as a hex string
+    # It's (PCP_BITS_PER_ROW / 8) bytes, and 2 characters per byte ('00' - 'ff')
+    format_str = '%.{}x'.format(int(PCP_BITS_PER_ROW / 8 * 2))
+    hash_as_str = ''.join([format_str % i for i in hash])
 
-    #os.system('rm %s/%s.png' % (get_pcp_hash_dir(), md5_hash))
+    with open(get_hash_filename(md5_hash), 'wt') as f:
+        f.write(hash_as_str)
+
+    os.system('rm %s/%s.png' % (get_pcp_hash_dir(), md5_hash))
 
     return hash_as_str
